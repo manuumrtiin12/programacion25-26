2b4093a83dff9d0a20175f6a64a48f8e577efdcb
manuumrtiin12 <mmarfer121d@g.educaand.es>
2025-12-03 09:13:17 +0100

    Miercoles 3 de Diciembre de 2025(Trabajo de clase, soraya falta)

--- Python/UD 2 - Programacion Modular/Boletin-EjercicioSimilaresII/Ejercicio1.py
+++ Python/UD 2 - Programacion Modular/Boletin-EjercicioSimilaresII/Ejercicio1.py
@@ -4,7 +4,6 @@ def generaNumeros():
 
     num1 = str(num1)
 
-    
     return num1, num2
 
 def valida(num1, num2):
--- Python/UD 2 - Programacion Modular/Boletin-EjercicioSimilaresII/Ejercicio3.py
+++ Python/UD 2 - Programacion Modular/Boletin-EjercicioSimilaresII/Ejercicio3.py
@@ -22,5 +22,57 @@ def binarioToDecimal():
                 print("El numero no es binario")
                 valido = False
 
+    resultado = 0
+    posicion = 0
+
+    for i in range(len(numero) -1, -1, -1):
+        digito = int(numero[i])
+        resultado += digito * 2**posicion
+        posicion += 1
+
+    return resultado
+
+def decimalToHexadecimal():
+    numero = int(input("Dame un numero entero: "))
+    hexadecimal = []
+
+    while numero > 0:
+        resto = numero % 16
+        numero //= 16
+
+        if resto < 10:
+            hexadecimal.append(resto)
         
-binarioToDecimal()
+        else:
+            if resto == 10:
+                hexadecimal.append("A")
+            
+            elif resto == 11:
+                hexadecimal.append("B")
+            
+            elif resto == 12:
+                hexadecimal.append("C")
+
+            elif resto == 13:
+                hexadecimal.append("D")
+
+            elif resto == 14:
+                hexadecimal.append("E")
+
+            elif resto == 15:
+                hexadecimal.append("F")
+        
+    hexadecimalBien = []
+
+    for i in range(len(hexadecimal)-1, -1, -1):
+        hexadecimalBien.append(hexadecimal[i])
+
+    cadenaHexadecimal = ""
+
+    for elemento in hexadecimalBien:
+        cadenaHexadecimal = cadenaHexadecimal + str(elemento)
+
+    return cadenaHexadecimal
+
+resultado = decimalToHexadecimal()
+print(resultado)
