8f2302e38aa8af03de6803525c20979400b0462e
manuumrtiin12 <mmarfer121d@g.educaand.es>
2025-12-10 09:53:08 +0100

    Miercoles 10 de Diciembre de 2025

---
+++ Java/Unidad1/src/main/java/Unidad2/Boletin1/Ejercicio10.java
@@ -0,0 +1,28 @@
+package Unidad2.Boletin1;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    static void main(String[] args) {
+
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.print("Dame un numero: ");
+        int numero1 = escaner.nextInt();
+
+        System.out.print("Dame otro numero: ");
+        int numero2 = escaner.nextInt();
+
+        float resultado = (float) numero1/numero2;
+
+        if (numero2 != 0) {
+            System.out.println("El resultado de la division es: " + resultado);
+
+        } else {
+            System.out.println("El segundo numero no puede ser cero");
+
+        }
+
+
+    }
+}
---
+++ Java/Unidad1/src/main/java/Unidad2/PruebasTransparencia.java
@@ -0,0 +1,28 @@
+package Unidad2;
+
+import java.util.Scanner;
+
+public class PruebasTransparencia {
+    static void main(String[] args) {
+
+        float y = 4.00F;
+        float x = 3.00F;
+        float resultado = y/x - y*x;
+
+        System.out.println("El resultado es " + resultado);
+
+        //
+
+        float numero = 3.12123F;
+        double decimalGrande = numero; //Casting Implicito por que NO pierde informacion
+        numero = (float) decimalGrande;
+        int entero = (int) numero;
+
+        Scanner escaner = new Scanner(System.in);
+        String cadena = escaner.next(); //Para un string
+
+        System.out.println(cadena);
+
+
+      }
+}
