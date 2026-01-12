49316708f9292e0a4248876c497efada96c77545
manuumrtiin12 <mmarfer121d@g.educaand.es>
2026-01-12 12:53:10 +0100

    Lunes 12 de Enero de 2026

--- Java/Unidad1/src/main/java/Unidad2/GremioDeMagos/GremioDeMagos.java
+++ Java/Unidad1/src/main/java/Unidad2/GremioDeMagos/GremioDeMagos.java
@@ -1,12 +1,37 @@
-package Unidad2.GremioDeMagos;
+package main.java.Unidad2.GremioDeMagos;
 
 import java.util.Scanner;
 
 public class GremioDeMagos {
 
-    public static String validarAcceso(String usuario, String contraseña)
+    public static boolean validarAcceso(String usuario, String contraseña, Scanner sc) {
 
-    static void main(String[] args) {
+        boolean contiene;
+
+        while (true) {
+
+            contiene = contraseña.contains("#") || contraseña.contains("*");
+
+            if (contraseña.length() < 8) {
+                System.out.println("La contraseña tiene que tener al menos 8 caracteres, inténtalo de nuevo...");
+            }
+            else if (!contiene) {
+                System.out.println("La contraseña tiene que contener # o *");
+            }
+            else {
+                return true;
+            }
+
+            System.out.print("Contraseña: ");
+            contraseña = sc.nextLine();
+        }
+    }
+
+    public static String cifradoDelConjuro(String conjuro) {
+        
+    }
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -16,6 +41,19 @@ public class GremioDeMagos {
         System.out.print("Contraseña: ");
         String contraseña = sc.nextLine();
 
+        if (validarAcceso(usuario, contraseña, sc)) {
+            System.out.println("Acceso concedido al gremio de magos, bienvenido " + usuario);
+        }
+
+        System.out.print("Cual es tu conjuro?: ");
+        String conjuro = sc.nextLine();
+
+
+        if (conjuro.contains("maldicion")) {
+            conjuro = conjuro.replace("maldicion", "bendicion");
+        }
+
+        System.out.println("Mensaje sin cifrar: " + conjuro);
 
     }
 }
