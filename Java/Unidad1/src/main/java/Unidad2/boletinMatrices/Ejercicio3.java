90c110805c008deca426ff0d652f98b3b8f6ccf3
manuumrtiin12 <mmarfer121d@g.educaand.es>
2026-01-21 23:20:55 +0100

    Miercoles 21 de Enero de 2026 (trabajo en casa)

--- Java/Unidad1/src/main/java/Unidad2/boletinMatrices/Ejercicio3.java
+++ Java/Unidad1/src/main/java/Unidad2/boletinMatrices/Ejercicio3.java
@@ -28,6 +28,48 @@ public class Ejercicio3 {
         }
     }
 
+    public void calcularEscudo(int [][] mapaEstelar) {
+        int escudo = 0;
+
+        for (int i = 0; i < mapaEstelar.length; i++) {
+            int[] fila = mapaEstelar[i];
+
+            for (int j = 0; j < fila.length; j++) {
+                escudo += mapaEstelar[i][j];
+            }
+        }
+
+        if (escudo >= 1000) {
+            System.out.println("Los escudos deben estar al 100%");
+        }
+        else {
+            System.out.println(" los escudos no deben estar al 100%");
+        }
+    }
+
+    public void encontrarRefugio(int[][] mapaEstelar) {
+        int refugio = 101;
+        int filaRefugio = -1;
+        int columnaRefugio = -1;
+
+        for (int i = 0; i < mapaEstelar.length; i++) {
+            for (int j = 0; j < mapaEstelar[i].length; j++) {
+                if (mapaEstelar[i][j] < refugio) {
+                    refugio = mapaEstelar[i][j];
+                    filaRefugio = i;
+                    columnaRefugio = j;
+                }
+            }
+        }
+
+        System.out.println(
+                "El lugar más seguro es " + refugio +
+                        " posicionado en fila: " + filaRefugio +
+                        " columna: " + columnaRefugio
+        );
+    }
+
+
     public static void main(String[] args) {
         Ejercicio3 e = new Ejercicio3();
         Scanner sc = new Scanner(System.in);
@@ -59,10 +101,10 @@ public class Ejercicio3 {
                 e.identificarAmenazas(mapaEstelar);
             }
             else if (eleccionMenu == 2) {
-
+                e.calcularEscudo(mapaEstelar);
             }
             else if (eleccionMenu == 3) {
-
+                e.encontrarRefugio(mapaEstelar);
             }
 
             e.menu(sc);
@@ -70,5 +112,7 @@ public class Ejercicio3 {
             System.out.print("Que quieres hacer? ");
             eleccionMenu = sc.nextInt();
         }
+
+        System.out.println("Saliendo...");
     }
 }
